6ed6814dafd52ba50e8e7ff5395a821f22375b26
Quoctuan1780 <Quoctuan1780@gmail.com>
2020-11-26 10:51:03 +0700

    First commit

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+var slider = document.querySelector('.scroll-horizontal');
+let isDown = false;
+let startX;
+let scrollLeft;
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft,
+        walk = (x - startX);
+    slider.scrollLeft = scrollLeft - walk;
+});
