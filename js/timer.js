0913ea599c02b8fa149f532b0a7c3fdb642cee3c
Quoctuan1780 <Quoctuan1780@gmail.com>
2020-11-27 17:09:02 +0700

    Hoan thanh anime sap chieu va anime moi

---
+++ js/timer.js
@@ -0,0 +1,23 @@
+function gettime(str_time, id) {
+    var countDownDate = new Date(str_time).getTime();
+
+    var x = setInterval(function() {
+
+        var now = new Date().getTime();
+
+        var distance = countDownDate - now;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById(id).innerHTML = days + "d " + hours + "h " +
+            minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById(id).innerHTML = "Sắp chiếu";
+        }
+    }, 1000);
+}
